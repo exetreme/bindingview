63bbe50466dcee9ef330da88911a96b3fa871a55
tunghvph12506 <tunghvph12506@fpt.edu.vn>
2021-05-06 21:44:11 +0700

    Initial commit a

---
+++ app/src/main/java/com/example/intenkolin/SeconActivity.kt
@@ -0,0 +1,11 @@
+package com.example.intenkolin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeconActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secon)
+    }
+}
